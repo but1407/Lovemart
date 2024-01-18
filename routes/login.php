47a4f061403faad36f83563085c799d83ceeaef5
but1407 <dinhtuanbut1472002@gmail.com>
2024-01-18 10:59:38 +0700

    modified:   app/Providers/RouteServiceProvider.php 	modified:   routes/login.php 	modified:   routes/web.php

--- routes/login.php
+++ routes/login.php
@@ -8,28 +8,28 @@ use App\Http\Controllers\Users\VerificationController;
 
 
 
-Route::prefix('admin/users')->group(function() {
-        #LoginController
-        Route::controller(LoginController::class)->group(function () {
-                Route::get('login','index')->name('login.index');
-                Route::post('login/store', 'store')->name('login.store');
-                #forgot password
-                Route::get('users/forgot-password', 'forgotPassword')->name('users.forgot-password');
-                Route::post('users/forgot-password','postForgotPass')->name('forgot-password');
-                Route::get('users/forgot-password/search-success','searchSuccess')->name('search-success');
-                Route::get('get-password/{customer}/{token}','getPass')->name('users.change-password');
-                Route::post('get-password/{customer}/{token}','postGetPass');
-        #register
-        Route::controller(AuthController::class)->group(function () {
-            Route::get('/index',  'index')->name('users.index');
-            Route::post('/register',  'register')->name('register');
-            Route::post('/re_register',  're_register');
+// Route::prefix('admin/users')->group(function() {
+//         #LoginController
+//         Route::controller(LoginController::class)->group(function () {
+//                 Route::get('login','index')->name('login.index');
+//                 Route::post('login/store', 'store')->name('login.store');
+//                 #forgot password
+//                 Route::get('users/forgot-password', 'forgotPassword')->name('users.forgot-password');
+//                 Route::post('users/forgot-password','postForgotPass')->name('forgot-password');
+//                 Route::get('users/forgot-password/search-success','searchSuccess')->name('search-success');
+//                 Route::get('get-password/{customer}/{token}','getPass')->name('users.change-password');
+//                 Route::post('get-password/{customer}/{token}','postGetPass');
+//         #register
+//         Route::controller(AuthController::class)->group(function () {
+//             Route::get('/index',  'index')->name('users.index');
+//             Route::post('/register',  'register')->name('register');
+//             Route::post('/re_register',  're_register');
             
-        });
-        Route::controller(VerificationController::class)->group(function () {
-            Route::get('email/verify/{id}', 'verify')->name('verification.verify');
-            Route::post('email/verify_OTP', 'verify_OTP')->name('verification.verify_OTP');
-            Route::post('email/logout_OTP', 'logout_OTP');
-        });
-    });
-});
+//         });
+//         Route::controller(VerificationController::class)->group(function () {
+//             Route::get('email/verify/{id}', 'verify')->name('verification.verify');
+//             Route::post('email/verify_OTP', 'verify_OTP')->name('verification.verify_OTP');
+//             Route::post('email/logout_OTP', 'logout_OTP');
+//         });
+//     });
+// });
